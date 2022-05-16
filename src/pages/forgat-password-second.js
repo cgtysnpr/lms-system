0cac4ce660a2d85e0e14f107873254b6b82852bb
cgtysnpr0 <cagatay.sinpari@bamep.com>
2022-05-16 05:41:49 +0300

    new changes

--- src/pages/forgat-password-second.js
+++ src/pages/forgat-password-second.js
@@ -1,73 +1,14 @@
+import { useState } from "react";
 const ForgatPasswordSecond = () => {
-  return (
-    <section className="background_color_grey pt-4">
-      <div className="container">
-        <div className="row mt-4 pb-5">
-          <div className="col-xl-6 offset-xl-3 pb-4">
-            <div className="text-center mb-4">
-              <h3 className="color_b_font_500 pb-2">Verify Your Email</h3>
-              <p>Please enter the OTP received on your email</p>
-              <p className="font-weight-bold">email@domain.com</p>
-            </div>
-            <div className="row py-3 pl-3">
-              <div className="col-2 p-md-3 p-0">
-                <div className="form-group ">
-                  <input
-                    className="form-control w-75  textfield text-center text-center"
-                    maxLength={1}
-                  />
-                </div>
-              </div>
-              <div className="col-2 p-md-3 p-0">
-                <div className="form-group ">
-                  <input
-                    className="form-control w-75  textfield text-center"
-                    maxLength={1}
-                  />
-                </div>
-              </div>
-              <div className="col-2 p-md-3 p-0">
-                <div className="form-group ">
-                  <input
-                    className="form-control w-75  textfield text-center"
-                    maxLength={1}
-                  />
-                </div>
-              </div>
-              <div className="col-2 p-md-3 p-0">
-                <div className="form-group ">
-                  <input
-                    className="form-control w-75  textfield text-center"
-                    maxLength={1}
-                  />
-                </div>
-              </div>
-              <div className="col-2 p-md-3 p-0">
-                <div className="form-group ">
-                  <input
-                    className="form-control w-75  textfield text-center"
-                    maxLength={1}
-                  />
-                </div>
-              </div>
-              <div className="col-2 p-md-3 p-0">
-                <div className="form-group ">
-                  <input
-                    className="form-control w-75  textfield text-center"
-                    maxLength={1}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="text-center pb-5">
-              <a href="#" className="btn btn-blue my-4">
-                Verify
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  const [step, setStep] = useState(0);
+  const handleChange = (event) => {
+    if (event.target.value) {
+      const form = event.target.form;
+      const index = [...form].indexOf(event.target);
+      form.elements[index + 1].focus();
+      event.preventDefault();
+    }
+  };
+  return <></>;
 };
 export default ForgatPasswordSecond;
